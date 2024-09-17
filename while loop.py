24f44afd87e0f96d96af0687085a7d93dbd21670
straightarrowhead <joshua.jemison20@gmail.com>
2024-09-16 19:56:58 -0500

    while loop.py
    this is a while loop. It will count up a user's score, and then once the program is done, it will display the total score and then display to the user that the program is finished running.

---
+++ while loop.py
@@ -0,0 +1,12 @@
+x = 0
+i = 10
+
+while i > 0:
+    print(x)
+    x += 10
+    print("There are " + str(i) + " loop(s) left.")
+    i -= 1
+
+print( "There are 0 loops left. The total number of x is " + str(x) + ".")
+
+print("this program is done.")
